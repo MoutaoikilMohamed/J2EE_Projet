b93b228eb0aa49348df501de6c989d178801acc8
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-04-11 12:54:45 +0000

    Add files via upload

---
+++ Reclamation/Tables.sql
@@ -0,0 +1,20 @@
+
+------------------------/*Database*/------------------------------
+
+
+CREATE DATABASE projectjavaswing;
+USE projectjavaswing
+
+------------------------/*Liste des Tables*/------------------------------
+
+
+
+
+/*Table Recuperation de mot de passe */
+
+CREATE TABLE recuperation (
+    CIN VARCHAR(10) PRIMARY KEY,
+    DTN DATE,
+    PROVINCE VARCHAR(25)
+);
+
