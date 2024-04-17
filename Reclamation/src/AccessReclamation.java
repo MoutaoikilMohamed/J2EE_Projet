dda642c4e7c9c34337b66418f809dca03dafbe06
hatimSa <153874008+hatimSa@users.noreply.github.com>
2024-04-17 06:55:36 +0100

    Add files via upload

---
+++ Reclamation/src/AccessReclamation.java
@@ -0,0 +1,129 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
+
+public class AccessReclamation extends JFrame{
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccessReclamation frame = new AccessReclamation();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AccessReclamation() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 872, 675);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 866, 82);
+		lblNewLabel.setIcon(new ImageIcon(AccessReclamation.class.getResource("/image/back.PNG")));
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Interface d'exploration");
+		lblNewLabel_1_1.setForeground(new Color(0, 147, 0));
+		lblNewLabel_1_1.setFont(new Font("Simplified Arabic", Font.BOLD, 18));
+		lblNewLabel_1_1.setBounds(23, 103, 231, 24);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Espace Gestionnaire");
+		lblNewLabel_1.setForeground(new Color(0, 196, 0));
+		lblNewLabel_1.setFont(new Font("Sitka Small", Font.BOLD, 17));
+		lblNewLabel_1.setBounds(336, 170, 231, 24);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(AccessReclamation.class.getResource("/image/Gestionnaire.png")));
+		lblNewLabel_2.setBounds(411, 93, 86, 66);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Liste des reclamations");
+		lblNewLabel_1_1_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1_1_1.setFont(new Font("Simplified Arabic", Font.BOLD, 18));
+		lblNewLabel_1_1_1.setBounds(364, 205, 231, 24);
+		contentPane.add(lblNewLabel_1_1_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(23, 272, 812, 171);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID", "Nom", "Type", "Localisation", "date cration", "date résolution", "Status", "CIN"
+			}
+		));
+		Connection conn = null;
+		try {
+		    conn = ConnectionDB.getConnection();
+
+		    String sql = "SELECT *from Reclamation";
+		    java.sql.Statement statement = conn.createStatement();
+		    ResultSet resultSet = statement.executeQuery(sql);
+
+		    DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+		    while (resultSet.next()) {
+		        int id = resultSet.getInt("ID");
+		        String nom = resultSet.getString("nom");
+		        String type = resultSet.getString("type");
+		        String localisation = resultSet.getString("localisation");
+		        Date date_creation = resultSet.getDate("date_creation"); 
+		        Date date_resolution = resultSet.getDate("date_resolution");
+		        String status = resultSet.getString("status");
+		        String CIN = resultSet.getString("CIN"); 
+
+		        model.addRow(new Object[]{id, nom, type, localisation, date_creation, date_resolution, status, CIN});
+		    }
+	
+		} catch (SQLException ex) {
+		    JOptionPane.showMessageDialog(null, "SQL Error: " + ex.getMessage());
+		} finally {
+		    ConnectionDB.closeConnection(conn);
+		}
+	}
+}
