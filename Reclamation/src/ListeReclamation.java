894c2d587c79f34c0849360dadfdd1b630225b15
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-05-09 19:15:16 +0100

    Update ListeReclamation.java

--- Reclamation/src/ListeReclamation.java
+++ Reclamation/src/ListeReclamation.java
@@ -22,9 +22,6 @@ public class ListeReclamation extends JFrame {
 	private JPanel contentPane;
 	private JTable table;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -38,15 +35,13 @@ public class ListeReclamation extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public ListeReclamation() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 872, 675);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -119,4 +114,6 @@ public class ListeReclamation extends JFrame {
 	}
 
 }
+
+
 
