247024b78d2671235477bd04a5e3e3bc49ffdba3
abdelhamidsoussi <122819227+abdelhamidsoussi@users.noreply.github.com>
2024-05-09 03:10:31 +0200

    Update Mes_Reclammation.java

--- Reclamation/src/Mes_Reclammation.java
+++ Reclamation/src/Mes_Reclammation.java
@@ -1,11 +1,7 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Mes_Reclammation extends JFrame {
 
@@ -26,97 +22,73 @@ public class Mes_Reclammation extends JFrame {
         });
     }
 
-
-
     public Mes_Reclammation(String CIN) {
         this.CIN = CIN;
         initialize();
         afficher_recl();
-	}
+    }
 
-	
-
-	private void initialize() {
-        this.setBounds(100, 100, 865, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Mes Réclamations");
-
+    private void initialize() {
+        setTitle("Mes Réclamations");
+        setBounds(100, 100, 865, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         contentPane = new JPanel();
-        contentPane.setLayout(null);
+        contentPane.setLayout(new BorderLayout());
         contentPane.setBackground(Color.WHITE);
-        this.setContentPane(contentPane);
+        setContentPane(contentPane);
 
         JLabel lblNewLabel = new JLabel(new ImageIcon(getClass().getResource("/image/back.PNG")));
-        lblNewLabel.setBounds(10, 0, 839, 89);
-        contentPane.add(lblNewLabel);
+        contentPane.add(lblNewLabel, BorderLayout.NORTH);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BorderLayout());
+        contentPane.add(centerPanel, BorderLayout.CENTER);
 
         JLabel lblNewLabel_1 = new JLabel(new ImageIcon(getClass().getResource("/image/Citoyen.PNG")));
-        lblNewLabel_1.setBounds(420, 116, 68, 59);
-        contentPane.add(lblNewLabel_1);
+        centerPanel.add(lblNewLabel_1, BorderLayout.NORTH);
 
         JLabel lblNewLabel_2 = new JLabel("Espace Citoyen");
         lblNewLabel_2.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 22));
-        lblNewLabel_2.setBounds(352, 186, 250, 30);
-        contentPane.add(lblNewLabel_2);
+        lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+        centerPanel.add(lblNewLabel_2, BorderLayout.CENTER);
 
         JLabel lblNewLabel_3 = new JLabel("Mes réclamations");
         lblNewLabel_3.setFont(new Font("Yu Gothic UI", Font.BOLD, 12));
-        lblNewLabel_3.setBounds(90, 230, 168, 14);
-        contentPane.add(lblNewLabel_3);
+        lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+        centerPanel.add(lblNewLabel_3, BorderLayout.SOUTH);
 
         table = new JTable();
         table.setModel(new DefaultTableModel(
-            new Object[][] {},
-            new String[] {"CIN", "Titre","Secteur", "Date Création","Sujet de reclammation", "Status"}
+                new Object[][]{},
+                new String[]{"CIN", "Titre", "Type", "Date Création","Date de Résolution","Sujet de réclamation", "Status"}
         ));
         scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(20, 250, 819, 200);
-        contentPane.add(scrollPane);
+        centerPanel.add(scrollPane, BorderLayout.CENTER);
     }
 
-	public void afficher_recl() {
-	    String fetchCIN = "SELECT CIN FROM users WHERE CIN=?";
-	    String fetchReclamations = "SELECT nom, idsec, date_creation, Description,status FROM Reclamation WHERE CIN=?";
-	    String fetchSecteur = "SELECT secteur FROM secteur_rec WHERE idsec=?";
-	    try (Connection conn = ConnectionDB.getConnection();
-	         PreparedStatement pstmtCIN = conn.prepareStatement(fetchCIN)) {
+    public void afficher_recl() {
+        String fetchReclamations = "SELECT nom, type, date_creation,date_resolution, Descreption, status FROM Reclamation WHERE CIN=?";
+        try (Connection conn = ConnectionDB.getConnection();
+             PreparedStatement pstmtRecl = conn.prepareStatement(fetchReclamations)) {
 
-	        pstmtCIN.setString(1, CIN);
-	        ResultSet rsCIN = pstmtCIN.executeQuery();
-	        if (rsCIN.next()) {
-	            String CIN = rsCIN.getString("CIN");
+            pstmtRecl.setString(1, CIN);
+            ResultSet rsRecl = pstmtRecl.executeQuery();
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            model.setRowCount(0);
 
-	            try (PreparedStatement pstmtRecl = conn.prepareStatement(fetchReclamations)) {
-	                pstmtRecl.setString(1, CIN);
-	                ResultSet rsRecl = pstmtRecl.executeQuery();
-	                DefaultTableModel model = (DefaultTableModel) table.getModel();
-	                model.setRowCount(0); 
+            while (rsRecl.next()) {
+                String titre = rsRecl.getString("nom");
+                String type = rsRecl.getString("type");
+                Date date = rsRecl.getDate("date_creation");
+                Date date1 = rsRecl.getDate("date_resolution");
+                String sujet = rsRecl.getString("Descreption");
+                String status = rsRecl.getString("status");
 
-	                while (rsRecl.next()) {
-	                    String titre = rsRecl.getString("nom");
-	                    int idSec = rsRecl.getInt("idsec");
-	                    Date date = rsRecl.getDate("date_creation");
-	                    String sujet = rsRecl.getString("Description");
-	                    String secteur = "";
-	                    String status=rsRecl.getString("status");
-
-	                    try (PreparedStatement pstmtSecteur = conn.prepareStatement(fetchSecteur)) {
-	                        pstmtSecteur.setInt(1, idSec);
-	                        ResultSet rsSecteur = pstmtSecteur.executeQuery();
-	                        if (rsSecteur.next()) {
-	                            secteur = rsSecteur.getString("secteur");
-	                        }
-	                    }
-
-	                    model.addRow(new Object[]{CIN, titre, secteur, date, sujet,status});
-	                }
-	            }
-	        } else {
-	            JOptionPane.showMessageDialog(this, "No CIN found for the username.", "Data Not Found", JOptionPane.ERROR_MESSAGE);
-	        }
-	    } catch (SQLException ex) {
-	        ex.printStackTrace();
-	        JOptionPane.showMessageDialog(this, "Error accessing the database: " + ex.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
-	    }
-	}
+                model.addRow(new Object[]{CIN, titre, type, date,date1,sujet, status});
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error accessing the database: " + ex.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
