568e450c61d2b6b05fd500249b65686bb26f2762
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-05-09 19:14:48 +0100

    Update ListeGestionnaire.java

--- Reclamation/src/ListeGestionnaire.java
+++ Reclamation/src/ListeGestionnaire.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 
@@ -24,9 +25,6 @@ public class ListeGestionnaire extends JFrame {
 	private JPanel contentPane;
 	private JTable table;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -40,12 +38,9 @@ public class ListeGestionnaire extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public ListeGestionnaire() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 872, 675);
+        setBounds(100, 100, 876, 675);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		contentPane = new JPanel();
@@ -56,15 +51,9 @@ public class ListeGestionnaire extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(0, 0, 866, 82);
+		lblNewLabel.setBounds(0, 0, 871, 82);
 		lblNewLabel.setIcon(new ImageIcon(ListeGestionnaire.class.getResource("/image/back.PNG")));
 		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Interface d'exploration");
-		lblNewLabel_1_1.setForeground(new Color(0, 147, 0));
-		lblNewLabel_1_1.setFont(new Font("Simplified Arabic", Font.BOLD, 18));
-		lblNewLabel_1_1.setBounds(23, 103, 231, 24);
-		contentPane.add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Espace Administrateur");
 		lblNewLabel_1.setForeground(new Color(0, 196, 0));
@@ -77,14 +66,14 @@ public class ListeGestionnaire extends JFrame {
 		lblNewLabel_2.setBounds(411, 93, 86, 66);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Liste des Gestionnaire");
+		JLabel lblNewLabel_1_1_1 = new JLabel("Liste des Agents administrative");
 		lblNewLabel_1_1_1.setForeground(new Color(0, 0, 0));
 		lblNewLabel_1_1_1.setFont(new Font("Simplified Arabic", Font.BOLD, 18));
-		lblNewLabel_1_1_1.setBounds(364, 205, 231, 24);
+		lblNewLabel_1_1_1.setBounds(323, 205, 292, 24);
 		contentPane.add(lblNewLabel_1_1_1);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 273, 805, 228);
+		scrollPane.setBounds(10, 273, 850, 352);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
@@ -93,11 +82,14 @@ public class ListeGestionnaire extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-					"CIN", "Nom", "Prénom", "Date de naissance", "Province","Ntel"
+					"CIN", "Nom", "Prénom", "Date de naissance", "Province","Ntel","Service"
 			}
 		));
+	    table.setRowHeight(table.getRowHeight() + 10);
+        JTableHeader header = table.getTableHeader();
+        header.setFont(header.getFont().deriveFont(Font.BOLD, 13)); 
 		
-		JButton btnNewButton = new JButton("Ajouter un Gestionneur");
+		JButton btnNewButton = new JButton("Ajouter un agent");
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(255, 128, 128));
 		btnNewButton.setFont(new Font("Sylfaen", Font.PLAIN, 13));
@@ -114,21 +106,23 @@ public class ListeGestionnaire extends JFrame {
 		try {
 		    conn = ConnectionDB.getConnection();
 
-		    String sql = "SELECT idusers, nom, prenom, date_naissance, province, Ntel FROM USERS WHERE Role ='Gestionnaire'";
+		    String sql = "SELECT CIN, nom, prenom, date_naissance, province, Ntel,Service FROM USERS WHERE Role ='Gestionneur'";
 		    java.sql.Statement statement = conn.createStatement();
 		    ResultSet resultSet = statement.executeQuery(sql);
 
 		    DefaultTableModel model = (DefaultTableModel) table.getModel();
 
 		    while (resultSet.next()) {
-		        String cin = resultSet.getString("idusers");
+		        String cin = resultSet.getString("CIN");
 		        String nom = resultSet.getString("nom");
 		        String prenom = resultSet.getString("prenom");
 		        String dateNaissance = resultSet.getString("date_naissance");
 		        String province = resultSet.getString("province");
 		        String NTel = resultSet.getString("Ntel");
+		        String Service = resultSet.getString("Service");
 
-		        model.addRow(new Object[]{cin, nom, prenom, dateNaissance, province,NTel});
+
+		        model.addRow(new Object[]{cin, nom, prenom, dateNaissance, province,NTel,Service});
 		    }
 	
 		} catch (SQLException ex) {
@@ -140,3 +134,4 @@ public class ListeGestionnaire extends JFrame {
 	}
 
 }
+
